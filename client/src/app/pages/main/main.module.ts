9cb52c775ce75619c6eab00e5efa6e835c0aad27
DmitrySidorukITRex <91536255+DmitrySidorukITRex@users.noreply.github.com>
2022-04-11 12:04:00 +0400

    add mat spinner

--- client/src/app/pages/main/main.module.ts
+++ client/src/app/pages/main/main.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CallHintComponent } from './call-hint/call-hint.component';
 import { EndGameModalComponent } from './end-game-modal/end-game-modal.component';
 import { HallHintComponent } from './hall-hint/hall-hint.component';
@@ -15,7 +16,12 @@ import { MainService } from './main.service';
     HallHintComponent,
     EndGameModalComponent,
   ],
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
   providers: [MainService],
 })
 export class MainModule {}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./app');
-const port = process.env.PORT || 5100;
+const port = process.env.PORT || 5200;
 
 app.listen(port, () => console.log(`Server has been started on ${port}`));
